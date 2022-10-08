bfc8aa6901ba1382de61ea720b4ec38df431e999
soyacen <soyacen@outlook.com>
2022-10-08 10:09:02 +0800

    /error.go add Err and  StatusCode function

--- error.go
+++ error.go
@@ -11,6 +11,14 @@ func NewHttpError(err error, statusCode int) *HttpError {
 	return &HttpError{err: err, statusCode: statusCode}
 }
 
+func (h HttpError) Err() error {
+	return h.err
+}
+
+func (h HttpError) StatusCode() int {
+	return h.statusCode
+}
+
 func (h HttpError) Error() string {
 	return fmt.Sprintf("http status code is %d, err: %v", h.statusCode, h.err)
 }
