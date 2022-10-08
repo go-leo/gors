c80542fd4ee8fe8250b2a0297bde9b39595b84a6
soyacen <soyacen@outlook.com>
2022-10-08 10:01:36 +0800

    /error.go add HttpError struct

---
+++ error.go
@@ -0,0 +1,16 @@
+package gors
+
+import "fmt"
+
+type HttpError struct {
+	err        error
+	statusCode int
+}
+
+func NewHttpError(err error, statusCode int) *HttpError {
+	return &HttpError{err: err, statusCode: statusCode}
+}
+
+func (h HttpError) Error() string {
+	return fmt.Sprintf("http status code is %d, err: %v", h.statusCode, h.err)
+}
