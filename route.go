ed557c54a3d7c932f8b398e0d44156480e8d2acb
soyacen <soyacen@outlook.com>
2022-12-01 21:58:30 +0800

    add IRoute interface

--- route.go
+++ route.go
@@ -2,8 +2,26 @@ package gors
 
 import "github.com/gin-gonic/gin"
 
+type IRoute interface {
+	Method() string
+	AbsolutePath() string
+	Handler() gin.HandlerFunc
+}
+
 type Route struct {
 	HTTPMethod  string
 	Path        string
 	HandlerFunc gin.HandlerFunc
+}
+
+func (r *Route) Method() string {
+	return r.HTTPMethod
+}
+
+func (r *Route) AbsolutePath() string {
+	return r.Path
+}
+
+func (r *Route) Handler() gin.HandlerFunc {
+	return r.Handler()
 }
