46db874e3a69cc31b529fb40591649d39bdd2aa9
soyacen <soyacen@outlook.com>
2023-01-05 10:10:59 +0800

    add rich route

--- route.go
+++ route.go
@@ -12,6 +12,30 @@ func NewRoute(method string, path string, handler gin.HandlerFunc) Route {
 	return &route{method: method, path: path, handler: handler}
 }
 
+func HandleRoutes(iRoutes gin.IRoutes, routes []Route) gin.IRoutes {
+	for _, route := range routes {
+		iRoutes = iRoutes.Handle(route.Method(), route.Path(), route.Handler())
+	}
+	return iRoutes
+}
+
+type RichRoute interface {
+	Method() string
+	Path() string
+	Handlers() []gin.HandlerFunc
+}
+
+func NewRichRoute(route Route, middlewares ...gin.HandlerFunc) RichRoute {
+	return &richRoute{route: route, middlewares: middlewares}
+}
+
+func HandleRichRoutes(iRoutes gin.IRoutes, routes []RichRoute) gin.IRoutes {
+	for _, route := range routes {
+		iRoutes = iRoutes.Handle(route.Method(), route.Path(), route.Handlers()...)
+	}
+	return iRoutes
+}
+
 type route struct {
 	method  string
 	path    string
@@ -30,9 +54,19 @@ func (r *route) Handler() gin.HandlerFunc {
 	return r.handler
 }
 
-func HandleRoutes(iRoutes gin.IRoutes, routes ...Route) gin.IRoutes {
-	for _, route := range routes {
-		iRoutes = iRoutes.Handle(route.Method(), route.Path(), route.Handler())
-	}
-	return iRoutes
+type richRoute struct {
+	route       Route
+	middlewares []gin.HandlerFunc
+}
+
+func (r *richRoute) Method() string {
+	return r.route.Method()
+}
+
+func (r *richRoute) Path() string {
+	return r.route.Path()
+}
+
+func (r *richRoute) Handlers() []gin.HandlerFunc {
+	return append(r.middlewares, r.route.Handler())
 }
