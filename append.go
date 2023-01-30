aa7aab1edbf5115aaabde7dbd8a7beef8efce4f4
soyacen <soyacen@outlook.com>
2023-01-30 13:50:58 +0800

    fix append method

--- append.go
+++ append.go
@@ -2,14 +2,14 @@ package gors
 
 import "github.com/gin-gonic/gin"
 
-func AppendRoutes[R gin.IRoutes](iRoutes R, routes []Route) R {
+func AppendRoutes[R gin.IRoutes](iRoutes R, routes ...Route) R {
 	for _, route := range routes {
 		_ = iRoutes.Handle(route.Method(), route.Path(), route.Handler())
 	}
 	return iRoutes
 }
 
-func AppendRichRoutes[R gin.IRoutes](iRoutes R, routes []RichRoute) R {
+func AppendRichRoutes[R gin.IRoutes](iRoutes R, routes ...RichRoute) R {
 	for _, route := range routes {
 		_ = iRoutes.Handle(route.Method(), route.Path(), route.Handlers()...)
 	}
