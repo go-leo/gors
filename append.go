173191f596f965623250d530effaf33f42c41316
soyacen <soyacen@outlook.com>
2023-06-02 17:30:21 +0800

    add some comma

--- append.go
+++ append.go
@@ -2,6 +2,7 @@ package gors
 
 import "github.com/gin-gonic/gin"
 
+// AppendRoutes 追加基础路由
 func AppendRoutes[R gin.IRoutes](iRoutes R, routes ...Route) R {
 	for _, route := range routes {
 		_ = iRoutes.Handle(route.Method(), route.Path(), route.Handler())
@@ -9,6 +10,7 @@ func AppendRoutes[R gin.IRoutes](iRoutes R, routes ...Route) R {
 	return iRoutes
 }
 
+// AppendRichRoutes 追加富路由
 func AppendRichRoutes[R gin.IRoutes](iRoutes R, routes ...RichRoute) R {
 	for _, route := range routes {
 		_ = iRoutes.Handle(route.Method(), route.Path(), route.Handlers()...)
--- error.go
+++ error.go
@@ -2,16 +2,12 @@ package gors
 
 import "fmt"
 
+// HttpError http 错误，
 type HttpError struct {
+	// statusCode http status code
 	statusCode int
-	err        error
-}
-
-func NewHttpError(statusCode int, err error) *HttpError {
-	return &HttpError{
-		statusCode: statusCode,
-		err:        err,
-	}
+	// err error message
+	err error
 }
 
 func (h HttpError) StatusCode() int {
@@ -20,4 +16,12 @@ func (h HttpError) StatusCode() int {
 
 func (h HttpError) Error() string {
 	return fmt.Sprintf("http status code is %d, err: %v", h.statusCode, h.err)
+}
+
+// NewHttpError 创建一个HttpError
+func NewHttpError(statusCode int, err error) *HttpError {
+	return &HttpError{
+		statusCode: statusCode,
+		err:        err,
+	}
 }
